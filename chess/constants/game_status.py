7fbf25525150e2def2ef996e7f492042e2ced5a3
Aditya Verma <meetadi3@gmail.com>
2020-04-12 16:54:07 +0530

    Remove chess

--- chess/constants/game_status.py
+++
@@ -1,5 +0,0 @@
-class GameStatus:
-    ACTIVE = 'active'
-    BLACK_WIN = 'black_win'
-    WHITE_WIN = 'white_win'
-    RESEIGNATION = 'resignation'
--- chess/models/interfaces/player_interface.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class PlayerInterface(ABC):
-    @abstractmethod
-    def is_white(self):
-        """Return whether the player is white or not""""
-        pass
