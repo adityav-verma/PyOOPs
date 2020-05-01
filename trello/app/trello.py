82df9d5f32d8c431bfaf882ec653e957d0c58e8a
Aditya Verma <meetadi3@gmail.com>
2020-05-01 18:40:04 +0530

    Add logic for API

--- trello/app/trello.py
+++ trello/app/trello.py
@@ -1,4 +1,7 @@
 from app.constants import BoardPrivacy
+from app.managers.board_list_manager import BoardListManager
+from app.managers.board_manager import BoardManager
+from app.managers.card_manager import CardManager
 
 
 class User(object):
@@ -6,30 +9,51 @@ class User(object):
 
 
 class Trello:
-    def create_board(self, name: str, privacy: BoardPrivacy) -> str: pass
+    def __init__(self):
+        # TODO: Use dependency injection here
+        self._board_manager = BoardManager()
+        self._list_manager = BoardListManager(self._board_manager)
+        self._card_manager = CardManager(self._list_manager)
 
-    def update_board(self, id: str, name: str, privacy: BoardPrivacy) -> str: pass
+    def create_board(self, name: str, privacy: BoardPrivacy) -> str:
+        return self._board_manager.create_board(name, privacy)
 
-    def show_board(self, id: str) -> dict: pass
+    def update_board(self, id: str, name: str, privacy: BoardPrivacy) -> str:
+        return self._board_manager.update_board(id, name, privacy)
 
-    def add_members_to_board(self, id: str, user: User) -> str: pass
+    def show_board(self, id: str) -> dict:
+        return self._board_manager.show_board(id)
 
-    def create_list(self, name: str, board_id: str) -> str: pass
+    def add_members_to_board(self, id: str, user: User) -> str:
+        return self._board_manager.add_members_to_board(id, user)
 
-    def show_list(self, id: str) -> dict: pass
+    def create_list(self, name: str, board_id: str) -> str:
+        return self._list_manager.create_list(name, board_id)
 
-    def create_card(self, name: str, description: str, list_id: str) -> str: pass
+    def show_list(self, id: str) -> dict:
+        return self._list_manager.show_list(id)
 
-    def assign_user_to_card(self, id: str, user: User) -> str: pass
+    def create_card(self, name: str, description: str, list_id: str) -> str:
+        return self._card_manager.create_card(name, description, list_id)
 
-    def show_card(self, id: str) -> dict: pass
+    def assign_user_to_card(self, id: str, user: User) -> str:
+        self._card_manager.assign_user_to_card(id, user)
 
-    def delete_board(self, id: str) -> None: pass
+    def show_card(self, id: str) -> dict:
+        return self._card_manager.show_card(id)
 
-    def delete_list(self, id: str) -> None: pass
+    def delete_board(self, id: str) -> None:
+        return self._board_manager.delete_board(id)
 
-    def delete_card(self, id: str) -> None: pass
+    def delete_list(self, id: str) -> None:
+        return self._list_manager.delete_list(id)
 
-    def remove_members_from_board(self, id: str, user: User) -> None: pass
+    def delete_card(self, id: str) -> None:
+        self._card_manager.delete_card(id)
 
-    def un_assign_user_from_card(self, id: str, user: User) -> None: pass
+    def remove_members_from_board(self, id: str, user: User) -> None:
+        self._board_manager.remove_members_from_board(id, user)
+
+    def un_assign_user_from_card(self, id: str, user: User) -> None:
+        self._card_manager.un_assign_user_from_card(id, user)
+
